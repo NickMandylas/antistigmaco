c7137510fd14ad82721e6630eb4a91a68d93f867
Nick Mandylas <nick.mandylas@gmail.com>
2020-12-17 07:30:54 +0000

    firrst commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./ANTISTIGMACO.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+        <p style={{ marginBottom: 0 }}>Coming soon in 2021.</p>
+        <p style={{ marginTop: 0 }}>
+          Visit <a href="https://sangfroidsociety.com">Sangfroid Society</a> (in
+          the meantime).
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
